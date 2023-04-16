7d1e7e68fb7963127a749a564f1a1b8c27449541
Reed Martin <reedhmartin@gmail.com>
2023-04-16 14:26:11 -0400

    Added gotos in index.js and both routes files.

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dayjs = require('dayjs');
+
+// TODO: Import API routes
+
+// TODO: Set up Express app and specify port
+
+// TODO: Set up middleware (body parser and CORS handling)
+
+// TODO: Connect to your MongoDB database using Mongoose
+
+// TODO: Set up API routes
+
+// TODO: Start the server and listen on the specified port
---
+++ routes/api/users/routes.js
@@ -0,0 +1,18 @@
+// TODO: Import required packages (Express)
+
+// TODO: Import User model
+
+// TODO: Create a new router instance from Express
+
+// TODO: Set up the API routes for users
+//       - GET all users
+//       - GET a single user by its _id and populate thought and friend data
+//       - POST a new user
+//       - PUT to update a user by its _id
+//       - DELETE to remove a user by its _id (bonus: remove user's associated thoughts when deleted)
+
+// TODO: Set up the API routes for a user's friend list
+//       - POST to add a new friend to a user's friend list
+//       - DELETE to remove a friend from a user's friend list
+
+// TODO: Export the router
