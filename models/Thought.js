4743f9dde95cc1fbbd7ba988b8e921ea88237a66
Reed Martin <reedhmartin@gmail.com>
2023-04-16 14:30:17 -0400

    Added todos to models files.

---
+++ models/Thought.js
@@ -0,0 +1,16 @@
+// TODO: Import required packages (Mongoose)
+
+// TODO: Define the Reaction schema (not a model, used as a subdocument schema for the Thought model)
+//       - reactionId (ObjectId, default value set to a new ObjectId)
+//       - reactionBody (String, required, 280 character maximum)
+//       - username (String, required)
+//       - createdAt (Date, default value set to the current timestamp, formatted using a getter method)
+
+// TODO: Define the Thought schema
+//       - thoughtText (String, required, 1 to 280 characters)
+//       - createdAt (Date, default value set to the current timestamp, formatted using a getter method)
+//       - username (String, required)
+//       - reactions (Array of nested documents created with the Reaction schema)
+//       - reactionCount (virtual property that retrieves the length of the thought's reactions array field on query)
+
+// TODO: Export the Thought model
---
+++ models/User.js
@@ -0,0 +1,10 @@
+// TODO: Import required packages (Mongoose)
+
+// TODO: Define the User schema
+//       - username (String, unique, required, trimmed)
+//       - email (String, required, unique, must match a valid email address)
+//       - thoughts (Array of _id values referencing the Thought model)
+//       - friends (Array of _id values referencing the User model, self-reference)
+//       - friendCount (virtual property that retrieves the length of the user's friends array field on query)
+
+// TODO: Export the User model
