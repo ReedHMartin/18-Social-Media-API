5f53f88879ec3a751e8f90e3b7b909300916bfba
Reed Martin <reedhmartin@gmail.com>
2023-04-16 15:26:58 -0400

    Implement User schema with username, email, thoughts, and friends fields

--- models/User.js
+++ models/User.js
@@ -3,12 +3,48 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
-// TODO: Define the User schema
-//       - username (String, unique, required, trimmed)
-//       - email (String, required, unique, must match a valid email address)
-//       - thoughts (Array of _id values referencing the Thought model)
-//       - friends (Array of _id values referencing the User model, self-reference)
-//       - friendCount (virtual property that retrieves the length of the user's friends array field on query)
+// Define the User schema
+const UserSchema = new Schema({
+    // Define the username field with the following constraints:
+    // - It should be a string
+    // - It should be unique (no two users with the same username)
+    // - It is required (cannot be empty)
+    // - Trimmed (removes leading and trailing white spaces)
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    // Define the email field with the following constraints:
+    // - It should be a string
+    // - It is required (cannot be empty)
+    // - It should be unique (no two users with the same email)
+    // - It must match a valid email address format
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+    },
+    // Define the thoughts field as an array of ObjectId values
+    // Each ObjectId in the array references a Thought document
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    // Define the friends field as an array of ObjectId values
+    // Each ObjectId in the array references a User document (self-reference)
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  });
+  
 
 // Export the User model
 const User = mongoose.model('User', UserSchema);
