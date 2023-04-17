b1887773df2ec6f327fadde129c43ee6f2501478
Reed Martin <reedhmartin@gmail.com>
2023-04-17 17:32:05 -0400

    Added import and export statements.

--- routes/api/users/routes.js
+++ routes/api/users/routes.js
@@ -1,20 +1,24 @@
-// TODO: Import required packages (Express)
+// Import required packages (Express)
+const express = require('express');
 
-// TODO: Import User model
+// Import userControllers
+const userControllers = require('../../controllers/userControllers');
 
-// TODO: Import userControllers
-
-// TODO: Create a new router instance from Express
+// Create a new router instance from Express
+const router = express.Router();
 
 // TODO: Set up the API routes for users using userControllers methods
-//       - GET all users
-//       - GET a single user by its _id and populate thought and friend data
-//       - POST a new user
-//       - PUT to update a user by its _id
-//       - DELETE to remove a user by its _id (bonus: remove user's associated thoughts when deleted)
+// - GET all users
+// - GET a single user by its _id and populate thought and friend data
+// - POST a new user
+// - PUT to update a user by its _id
+// - DELETE to remove a user by its _id (bonus: remove user's associated thoughts when deleted)
 
 // TODO: Set up the API routes for a user's friend list using userControllers methods
-//       - POST to add a new friend to a user's friend list
-//       - DELETE to remove a friend from a user's friend list
+// - POST to add a new friend to a user's friend list
+// - DELETE to remove a friend from a user's friend list
 
-// TODO: Export the router
+// Export the router
+module.exports = router;
+
+
